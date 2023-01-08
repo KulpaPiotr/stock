1d1f619349abdcff83c2e20923f8a482a1978ad4
KulpaPiotr <piokul83@gmail.com>
2023-01-08 20:29:18 +0100

    initial

---
+++ src/test/java/pl/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
