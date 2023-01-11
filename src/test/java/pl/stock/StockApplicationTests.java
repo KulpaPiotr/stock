0001eab44a1ed9d40f67a8c1621d2fd37f3511b6
KulpaPiotr <piokul83@gmail.com>
2023-01-11 16:07:18 +0100

    DatabaseConfiguration

--- src/test/java/pl/stock/StockApplicationTests.java
+++ src/test/java/pl/stock/StockApplicationTests.java
@@ -2,6 +2,7 @@ package pl.stock;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 class StockApplicationTests {
