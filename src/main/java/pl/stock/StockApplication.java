558cc803e1a7fe9e760edb31bacba5b3dd078ad0
KulpaPiotr <piokul83@gmail.com>
2023-01-11 12:57:59 +0100

    profiles

--- src/main/java/pl/stock/StockApplication.java
+++ src/main/java/pl/stock/StockApplication.java
@@ -2,9 +2,15 @@ package pl.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StockApplication {
+public class StockApplication extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StockApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StockApplication.class, args);
